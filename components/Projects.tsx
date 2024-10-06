b717c57eb5bb9f8d97ba547348af5d68c557b264
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-10-06 15:08:17 +0500

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { Button } from "@/components/ui/button";
 import { FaExternalLinkSquareAlt } from "react-icons/fa";
 import { FaArrowAltCircleUp } from "react-icons/fa";
+import { motion } from 'framer-motion';
 
 const Projects = () => {
 
