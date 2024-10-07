709df8e41e25908db4677f34e7eb6dfaebe8e324
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-10-07 10:08:33 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,34 +4,39 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className='bg-emerald-950 flex flex-row justify-between items-center p-2'>
+    <header className='bg-emerald-950 flex flex-row justify-between items-center p-1 lg:p-2'>
         <Link href="/">
         <Image
         src="/profile1.jpeg"
         alt='Taha Siraj'
-        width={80}
+        width={70}
         height={100}
         className='rounded-full active:scale-95 transition-transform transform duration:700'
         />
         </Link>
-        <ul className='flex gap-2 md:gap-3 justify-center items-center mr-2 font-bold'>
+        <ul className='flex gap-2 md:gap-3 justify-center items-center mr-2 font-thin lg:font-bold'>
+        <Link href="/">
+            <li className='text-green-400 lg:text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
+                Home
+            </li>
+            </Link>
             <Link href="/about">
-            <li className='text-green-400 text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
+            <li className='text-green-400 lg:text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
                 About
             </li>
             </Link>
             <Link href="/projects">
-            <li className='text-green-400 text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
+            <li className='text-green-400 lg:text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
                 Projects
             </li>
             </Link>
             <Link href="/skills">
-            <li className='text-green-400 text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
+            <li className='text-green-400 lg:text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
                 Skills
             </li>
             </Link>
             <Link href="/contact">
-            <li className='text-green-400 text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
+            <li className='text-green-400 lg:text-lg hover:scale-110 active:scale-105 transition-transform transform duration:500'>
                 Contact
             </li>
             </Link>
@@ -41,4 +46,3 @@ const Header = () => {
 }
 
 export default Header
-
