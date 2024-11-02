3553fbd6e84c08816ac6d291a5919452dffde3e8
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-11-03 03:00:13 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,9 @@
-"use client";
 import React from 'react';
 import { Dot, Linkedin, Instagram, Github } from 'lucide-react';
-import { Button } from "@/components/ui/button";
+import { FaWhatsapp } from "react-icons/fa";
+import Link from 'next/link';
 
 const Footer = () => {
-
-  const handleProfileViews = (Link: string): void => {
-    window.open(Link);
-  };
-
   return (
     <footer className='font-sans flex flex-col justify-center items-center h-20 bg-emerald-800'>
         <div className='flex flex-row pt-2 font-semibold'>
@@ -16,28 +11,27 @@ const Footer = () => {
         <Dot /><span>Taha Siraj</span>
         <Dot /><span>All rights reserved</span>
         </div>
-        <div className='flex flex-row'>
-          <Button
-          onClick={() => handleProfileViews("https://www.linkedin.com/in/taha-siraj-521b512b7/")}
-          className='bg-inherit hover:bg-transparent'
-          >
-            <Linkedin className='hover:text-black active:scale-95 transition-transform transform duration-300' />
-          </Button>
-          <Button
-          onClick={() => handleProfileViews("https://github.com/Tahasiraj1")}
-          className='bg-inherit hover:bg-transparent'
-          >
-            <Github  className='hover:text-black active:scale-95 transition-transform transform duration-300 ' />
-          </Button>
-          <Button
-          onClick={() => handleProfileViews("https://www.instagram.com/taha__siraj/")}
-          className='bg-inherit hover:bg-transparent'
-          >
-            <Instagram className='hover:text-black active:scale-95 transition-transform transform duration-300 ' />
-          </Button>
+        <div className='flex flex-row mt-2 gap-10 mb-2'>
+
+        <Link href="https://www.linkedin.com/in/taha-siraj-521b512b7/" target='_blank'>
+            <Linkedin size={26} className='hover:text-black text-white active:scale-95 transition-transform transform duration-300 ' />
+          </Link>
+
+          <Link href="https://www.instagram.com/taha__siraj/" target='_blank'>
+            <Instagram size={26} className='hover:text-black text-white active:scale-95 transition-transform transform duration-300 ' />
+          </Link>
+
+          <Link href="https://github.com/Tahasiraj1" target='_blank'>
+            <Github size={26} className='hover:text-black text-white active:scale-95 transition-transform transform duration-300 ' />
+          </Link>
+
+          <Link href="https://wa.me/923311245238" target='_blank'>
+            <FaWhatsapp size={26} className='hover:text-black text-white active:scale-95 transition-transform transform duration-300 ' />
+          </Link>
+
         </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
