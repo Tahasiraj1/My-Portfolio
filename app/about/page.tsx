f391bb71efb97da1e5fae75236371af7837f910c
Tahasiraj1 <tahasiraj242@gmail.com>
2024-11-08 14:54:10 +0500

    again abot page fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import About from '@/components/About'
 
 const about = () => {
   return (
-    <div className='bg-gradient-to-b from-green-300 to-green-400 h-screen'>
+    <div className='bg-gradient-to-b from-green-300 to-green-400'>
       <About />
     </div>
   )
