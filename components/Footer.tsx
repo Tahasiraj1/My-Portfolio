8c5b7360813ba724bbfc1792b6dbc9cc8e62b3ad
Tahasiraj1 <tahasiraj242@gmail.com>
2025-06-07 19:52:16 +0500

    style: change footer background color to black and update copyright text color for improved visibility

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ import { AnimatedShinyText } from "./ui/AnimatedShinyText";
 
 const Footer = () => {
   return (
-    <footer className='bg-neutral-950'>
+    <footer className='bg-black'>
       <div className="container mx-auto px-4 py-4">
         <div className="text-6xl sm:text-7xl md:text-8xl font-extrabold text-center mb-12 text-gray-400 opacity-20">
             <AnimatedShinyText>TAHA SIRAJ</AnimatedShinyText>
         </div>
         
         <div className="mt-12 pt-8 border-t border-slate-700/40 text-center text-sm">
-          <p>&copy; {new Date().getFullYear()}. All rights reserved.</p>
+          <p className='text-gray-600'>&copy; {new Date().getFullYear()}. All rights reserved.</p>
         </div>
       </div>
     </footer>
